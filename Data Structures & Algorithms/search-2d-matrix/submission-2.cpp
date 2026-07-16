33412ea2b1fd64118c8205d79c3c76c89c84fa11
Hector-lg <hlugogabino@gmail.com>
2026-07-15 21:19:45 -0600

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+    for(int i = 0; i<matrix.size(); i++){
+    int left = 0;
+    int right = matrix[0].size() -1;
+        if(matrix[i][left] <= target && matrix[i][right] >= target){
+            while(left<=right){
+                int mid = left + (right-left)/2;
+        if(matrix[i][mid] == target){
+            return true;
+        }
+        else if(matrix[i][mid]< target){
+            left = mid+1;
+        }
+        else if(matrix[i][mid]> target){
+            right = mid-1;
+        }
+            }
+        }
+    }
+    return false;
+    }
+};
